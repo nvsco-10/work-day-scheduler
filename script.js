a170ca6537ac4a68c37af3f21b4a0e693471997c
Neema Velasco <neemavelasco@gmail.com>
2022-03-12 11:53:59 -0600

    added for loop to create one block per hour 9am - 5pm

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const currentDayOutput = $("#currentDay");
 const blocksContainer = $(".container");
+const startBlock = 9 // 9AM - 5PM
 
 // current date
 const today = moment();
@@ -10,12 +11,18 @@ currentDayOutput.textContent = today.format("dddd, MMMM Do");
 $(document).ready(loadTimeBlocks);
 
 function loadTimeBlocks() {
-    const blockRow = $(`<div class="row time-block">`)
-    blockRow.append(`<div class='col-lg-2 hour'>9AM</div>`)
-    blockRow.append(`<div class='col-lg-8'><textarea name='input-text' id='input-text'></textarea></div>`)
-    blockRow.append(`<div class="col-lg-2 "><button class="saveBtn"><i class="fa-solid fa-floppy-disk"></i></button></div>`)
 
-    blocksContainer.append(blockRow);
+    // 9 - 17 will create 9 blocks
+    for(let i=startBlock; i<=17; i++) {
+        const blockRow = $(`<div class="row time-block">`);
+
+        blockRow.append(`<div class='col-lg-2 hour'>9AM</div>`)
+        blockRow.append(`<div class='col-lg-8'><textarea name='input-text' id='input-text'></textarea></div>`)
+        blockRow.append(`<div class="col-lg-2 "><button class="saveBtn"><i class="fa-solid fa-floppy-disk"></i></button></div>`)
+
+        blocksContainer.append(blockRow);
+    }
+
 }
 
 
