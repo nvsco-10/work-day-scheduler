c1217b391290cb3c53c1c1af3bd03530c9ea7794
Neema Velasco <neemavelasco@gmail.com>
2022-03-12 11:20:04 -0600

    added time-block row and added existing classes listed in css

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const currentDayOutput = document.querySelector("#currentDay");
+const blocksContainer = document.querySelector(".container");
 
+// current date
 const today = moment();
 
+// display current date - format: Saturday, March 12th
 currentDayOutput.textContent = today.format("dddd, MMMM Do");
+
